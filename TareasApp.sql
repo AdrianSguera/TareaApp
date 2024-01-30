12911cfb82c1e05188cb0cf9c4907c18be6f6fad
Adrian_Sguera <adrianfsguera@gmail.com>
2024-01-30 17:23:18 +0100

    Agregada base de datos

---
+++ TareasApp.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tareasapp
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `idrol` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  PRIMARY KEY (`idrol`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'user'),(2,'admin');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `idtask` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `creation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deadline` datetime NOT NULL,
+  `status` varchar(45) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `iduser` int NOT NULL,
+  PRIMARY KEY (`idtask`),
+  KEY `tasks_users_idx` (`iduser`),
+  CONSTRAINT `tasks_users` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'trabajo','hacer algo','2024-01-30 17:07:29','2024-01-30 15:25:00','unfinished',2),(2,'tarea','hacer algo','2024-01-30 17:07:29','2024-03-22 15:25:00','unfinished',2),(3,'algo','hacer algo','2024-01-30 17:07:29','2024-02-10 15:25:00','unfinished',3);
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `iduser` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `pasword` varchar(45) COLLATE utf8mb3_spanish2_ci NOT NULL,
+  `idrol` int NOT NULL,
+  PRIMARY KEY (`iduser`),
+  KEY `users_roles_idx` (`idrol`),
+  CONSTRAINT `users_roles` FOREIGN KEY (`idrol`) REFERENCES `roles` (`idrol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','1234',2),(2,'user1','1234',1),(3,'user2','1234',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-30 17:22:25
--- src/com/ceica/modelos/Task.java
+++ src/com/ceica/modelos/Task.java
@@ -1,6 +1,19 @@
 package com.ceica.modelos;
 
+import java.sql.Timestamp;
+
 public class Task {
     private String title, description, status;
-    private
+    private Timestamp creation_time, deadline;
+
+    public Task() {
+    }
+
+    public Task(String title, String description, String status, Timestamp deadline) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.creation_time = new Timestamp(System.currentTimeMillis());
+        this.deadline = deadline;
+    }
 }
