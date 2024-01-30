ecdc3c46a02f9fbd23a5aca7d2db7f75f6cd4a98
Adrian_Sguera <adrianfsguera@gmail.com>
2024-01-30 17:14:52 +0100

    Inicio de proyecto

---
+++ src/com/ceica/modelos/Task.java
@@ -0,0 +1,6 @@
+package com.ceica.modelos;
+
+public class Task {
+    private String title, description, status;
+    private
+}
---
+++ src/com/ceica/modelos/User.java
@@ -0,0 +1,48 @@
+package com.ceica.modelos;
+
+public class User {
+    private String username, password;
+    private int rol;
+
+    public User() {
+    }
+
+    public User(String username, String password, int rol) {
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", rol=" + rol +
+                '}';
+    }
+}
